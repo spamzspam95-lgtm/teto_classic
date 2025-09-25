a9ad68a9e6b890544d36fa74e6f38ce160c34862
spamzspam95-lgtm <spamzspam95@gmail.com>
2025-09-25 01:44:48 -0700

    Update main.js
    تعديل الكود

--- main.js
+++ main.js
@@ -121,3 +121,13 @@ if (orderForm) {
     closeModal();
   });
 }
+function showSection(id) {
+  // أخفي كل الأقسام
+  document.getElementById('shirts').style.display = 'none';
+  document.getElementById('pants').style.display = 'none';
+  document.getElementById('shoes').style.display = 'none';
+
+  // أظهر القسم المطلوب
+  document.getElementById(id).style.display = 'block';
+}
+
